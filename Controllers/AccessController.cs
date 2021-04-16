d8e3d00acc759d8fbf6227f2250ef543ab96cf68
Javier Aguirre <aguirrecarlos1995@gmail.com>
2021-04-16 12:52:16 -0300

    das

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CursoMVC.Models;
+namespace CursoMVC.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string pass)
+        {
+
+            try
+            {
+                using (CursomvcEntities1 db = new CursomvcEntities1())
+                {
+                    var lista = from d in db.User
+                                where d.correo == user && d.password == pass && d.idState == 1
+                                select d;
+                    if (lista.Count() > 0)
+                    {
+                        User oUser = lista.First();
+                        Session["User"] = oUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario invalido..");
+                    }
+
+                }
+
+           
+            }
+            catch(Exception ex)
+            {
+                return Content("Ocurrio un error " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Filtros/VerificarSesion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CursoMVC.Controllers;
+using CursoMVC.Models;
+using System.Web.Routing;
+
+namespace CursoMVC.Filtros
+{
+    public class VerificarSesion : ActionFilterAttribute
+
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var oUser = (User)HttpContext.Current.Session["User"];
+
+            if(oUser == null)
+            {
+                if(filterContext.Controller is AccessController == false)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Access/Index");
+                }
+               
+            }
+            else
+            {
+                if (filterContext.Controller is AccessController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home/Index");
+                }
+            }
+        }
+    }
+}
